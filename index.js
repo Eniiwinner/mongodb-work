1f853cca8a7b83513fbce195095456da1d91140a
Eniiwinner <abrahameniola624@gmail.com>
2025-04-03 02:08:53 -0500

    bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const mongoURI = 'mongodb+srv://eniiwinner:<db_password>@cluster0.ds4fi1q.mongodb.net/devops?retryWrites=true&w=majority'
+const mongoURI = 'mongodb+srv://eniiwinner:<8KE076mhgYc0t8cS>@cluster0.ds4fi1q.mongodb.net/devops?retryWrites=true&w=majority'
 
 mongoose.connect('mongodb://localhost/devops', { useNewUrlParser: true, useUnifiedTopology: true });
 
